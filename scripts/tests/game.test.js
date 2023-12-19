ab90bf2604e597cf77b94528f68b0dd467a883a6
bezabu <>
2023-12-19 16:01:19 +0000

    extending tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,7 +5,7 @@
 const { game } = require("../game");
 
 
-beforeEach(() => {
+beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
@@ -17,13 +17,16 @@ describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
     });
-    test("current game key exists", () => {
+    test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
     });
-    test("player moves key exists", () => {
+    test("playerMoves key exists", () => {
         expect("playerMoves" in game).toBe(true);
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
