463e28ce80e824bbac559882147f10760bc5210c
bezabu <>
2023-12-19 15:55:00 +0000

    basic tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,3 +2,28 @@
  * @jest-environment jsdom
  */
 
+const { game } = require("../game");
+
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("current game key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("player moves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+});
